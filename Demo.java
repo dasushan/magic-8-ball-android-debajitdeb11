b5c7a6c0539e78427c2adc48f0ffb57b438e5c07
dasushan <53913896+dasushan@users.noreply.github.com>
2020-05-22 20:16:04 +0530

    Create Demo.java
    /*  github demo1
         obj method call java */

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+        public void myMethod(){
+            System.out.println("Hello World");
+            }
+            
+public static void main(String[] args) {
+        Demo myObj = new Demo();
+        myObj.Demo();
+        }
+      }
